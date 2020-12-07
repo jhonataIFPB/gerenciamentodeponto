065ac2e52302289af66887e9314e1557b814ff6c
jhonataIFPB <56457065+jhonataIFPB@users.noreply.github.com>
2020-12-07 09:37:00 -0300

    Add files via upload

--- projeto 1/js/login.js
+++ projeto 1/js/login.js
@@ -2,10 +2,10 @@ function logar() {
     var cpf = document.getElementById("cpf");
     var senha = document.getElementById("senha")
 
-    if (cpf.value == "00000000000" && senha.value == "admin") {
-        localStorage.setItem("acesso", true);
+    if (cpf.Value == "11111111111" && senha.Value == "admin") {
+        localStorage.getItem("acesso", true);
 
-        window.location.href = "Controledeponto.html";
+        window.location.href = "Controle de ponto.html";
     } else {
         alert("Funcionario não cadastrado consulte os dados ou entre em contato com o admin")
     }
